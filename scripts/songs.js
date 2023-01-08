788246e07bce5f3b468ae5badd6ec108d0e033c9
kate <katezraly@gmail.com>
2023-01-07 17:29:36 -0800

    added spotify embeds to each card

--- scripts/songs.js
+++ scripts/songs.js
@@ -7,13 +7,18 @@ var cardTemplate = document.getElementById("cardTemplate");
 
 
 // Injects a single card into the gallery
-function displayCard(idIndex, nonTranslated, translated, spotify) {
+function displayCard(idIndex, nonTranslated, translated) {
 
     let newCard = cardTemplate.content.cloneNode(true);
 
     newCard.id = "card" + idIndex;
 
-    newCard.querySelector('.card-title').innerHTML = nonTranslated + " - " + translated;
+    if (nonTranslated == translated)
+        newCard.querySelector('.card-title').innerHTML = nonTranslated;
+    else
+        newCard.querySelector('.card-title').innerHTML = nonTranslated + " - " + translated;
+
+    newCard.querySelector('.spotify-embed').src = "https://open.spotify.com/embed/track/5QDLhrAOJJdNAmCTJ8xMyW?utm_source=generator&theme=0";
 
     gallery.appendChild(newCard);
 
@@ -21,13 +26,21 @@ function displayCard(idIndex, nonTranslated, translated, spotify) {
 
 function displaySongs() {
 
-    for (let i = 0; i < 10; i++) {
-        
-        displayCard(i, "Card " + i, "XXX " + i);
-    
-    }
+    // Loads in the cards when the user first initializes the page
+    db.collection("songs")
+    //.where("language", "==", "Korean")
+    .get()
+        .then(songs => {
+
+            songs.forEach(song => {
+
+                displayCard(song.data().songid, song.data().title, song.data().translation);
+
+            });
+
+        });
 
 }
 
-    
+displaySongs();    
 
